aff7a41f6bf10290a52921af378fb5292d0fe5db
Ryder James <stidwell@student.neumont.edu>
2020-08-31 15:47:30 -0600

    Init commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require(`express`);
+const path = require(`path`);
+const bodyParser = require(`body-parser`)
+const expressSession = require(`express-session`);
+const routes = require(`./routes/routes`);
+const secret = require(`./secret`);
+
+const app = express();
+
+app.use((req, res, next) => {
+	res.header(`Access-Control-Allow-Origin`, `*`);
+	res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept`);
+	next();
+});
+
+let urlencodedParser = bodyParser.urlencoded({
+	extended: true
+});
+
+const checkAuth = (req, res, next) => {
+	if (req.session.user && req.session.user.isAuthenticated) {
+		next();
+	} else {
+		res.redirect(`/`);
+	}
+}
+
+app.set(`view engine`, `pug`);
+app.set(`views`, path.join(__dirname, `/views`));
+app.use(express.static(path.join(__dirname, `/public`)));
+
+app.use(expressSession({
+	secret: secret["session-pass"],
+	saveUninitialized: true,
+	resave: true
+}));
+
+app.get(`/`, routes.index);
+
+app.listen(3000);
---
+++ routes/routes.js
@@ -0,0 +1,21 @@
+const mongoose = require(`mongoose`);
+const secret = require(`../secret`);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(`mongodb+srv://rjames:${secret['mongo-pass']}@comcharacters.ep4vw.mongodb.net/CoMCharacters?retryWrites=true&w=majority`, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true
+});
+
+let mdb = mongoose.connection;
+mdb.on(`error`, console.error.bind(console, `connection error`));
+mdb.once(`open`, callback => {
+	
+});
+
+exports.index = (req, res) => {
+	res.render(`index`, {
+		title: "Home"
+	});
+};
